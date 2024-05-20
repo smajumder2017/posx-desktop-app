9f3c478a548febe5b8a08e5cb52087f870244ef5
Subhadip Majumder <majumdersubhadip.sm@gmail.com>
2024-05-20 13:21:09 +0530

    fix migration issue in packaging

---
+++ apps/backend/electron/constants.ts
@@ -0,0 +1,84 @@
+import * as path from 'path';
+import { app } from 'electron';
+import * as os from 'os';
+
+function getDBPath() {
+  const platform = os.platform();
+  switch (platform) {
+    case 'darwin': {
+      return path.join(app.getPath('appData'), '..', 'posx', 'data', 'posx.db');
+    }
+    case 'win32': {
+      return path.join(app.getPath('appData'), 'posx', 'data', 'posx.db');
+    }
+
+    default:
+      return process.env.DATABASE_URL!;
+  }
+}
+
+export const isDev = !app.isPackaged;
+export const dbPath = getDBPath();
+export const dbUrl = isDev ? process.env.DATABASE_URL! : 'file:' + dbPath;
+console.log(dbPath);
+// Hacky, but putting this here because otherwise at query time the Prisma client
+// gives an error "Environment variable not found: DATABASE_URL" despite us passing
+// the dbUrl into the prisma client constructor in datasources.db.url
+process.env.DATABASE_URL = dbUrl;
+
+// This needs to be updated every time you create a migration!
+export const latestMigration = '20240519053810_add_payment_table';
+export const platformToExecutables: any = {
+  win32: {
+    migrationEngine: 'node_modules/@prisma/engines/schema-engine-windows.exe',
+    queryEngine: 'node_modules/@prisma/engines/query_engine-windows.dll.node',
+  },
+  linux: {
+    migrationEngine:
+      'node_modules/@prisma/engines/schema-engine-debian-openssl-1.1.x',
+    queryEngine:
+      'node_modules/@prisma/engines/libquery_engine-debian-openssl-1.1.x.so.node',
+  },
+  darwin: {
+    migrationEngine: 'node_modules/@prisma/engines/schema-engine-darwin',
+    queryEngine:
+      'node_modules/@prisma/engines/libquery_engine-darwin.dylib.node',
+  },
+  darwinArm64: {
+    migrationEngine: 'node_modules/@prisma/engines/schema-engine-darwin-arm64',
+    queryEngine:
+      'node_modules/@prisma/engines/libquery_engine-darwin-arm64.dylib.node',
+  },
+};
+const extraResourcesPath = app.getAppPath().replace('app.asar', ''); // impacted by extraResources setting in electron-builder.yml
+
+function getPlatformName(): string {
+  const isDarwin = process.platform === 'darwin';
+  if (isDarwin && process.arch === 'arm64') {
+    return process.platform + 'Arm64';
+  }
+
+  return process.platform;
+}
+
+const platformName = getPlatformName();
+
+export const mePath = path.join(
+  extraResourcesPath,
+  platformToExecutables[platformName].migrationEngine,
+);
+export const qePath = path.join(
+  extraResourcesPath,
+  platformToExecutables[platformName].queryEngine,
+);
+
+export interface Migration {
+  id: string;
+  checksum: string;
+  finished_at: string;
+  migration_name: string;
+  logs: string;
+  rolled_back_at: string;
+  started_at: string;
+  applied_steps_count: string;
+}
--- apps/backend/electron/main.ts
+++ apps/backend/electron/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { app, BrowserWindow, shell } from 'electron';
-import { exec } from 'child_process';
-
+// import { exec } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+import { dbPath, dbUrl, latestMigration, Migration } from './constants';
+import { prisma, runPrismaCommand } from './prisma';
 import log from 'electron-log/main';
 const isDev = !app.isPackaged;
 
@@ -19,33 +22,87 @@ const createWindow = async () => {
   try {
     log.info('Log from the main process');
 
+    log.info(`Mode:  ${isDev ? 'dev' : 'prod'}`);
+    if (!isDev) {
+      let needsMigration;
+      const dbExists = fs.existsSync(dbPath);
+      if (!dbExists) {
+        needsMigration = true;
+        // prisma for whatever reason has trouble if the database file does not exist yet.
+        // So just touch it here
+        fs.closeSync(fs.openSync(dbPath, 'w'));
+      } else {
+        try {
+          const latest: Migration[] =
+            await prisma.$queryRaw`select * from _prisma_migrations order by finished_at`;
+          needsMigration =
+            latest[latest.length - 1]?.migration_name !== latestMigration;
+        } catch (e) {
+          log.error(e);
+          needsMigration = true;
+        }
+      }
+
+      if (needsMigration) {
+        try {
+          const schemaPath = path.join(
+            app.getAppPath().replace('app.asar', 'app.asar.unpacked'),
+            'apps',
+            'backend',
+            'prisma',
+            'schema.prisma',
+          );
+          log.info(
+            `Needs a migration. Running prisma migrate with schema path ${schemaPath}`,
+          );
+
+          // first create or migrate the database! If you were deploying prisma to a cloud service, this migrate deploy
+          // command you would run as part of your CI/CD deployment. Since this is an electron app, it just needs
+          // to run every time the production app is started. That way if the user updates the app and the schema has
+          // changed, it will transparently migrate their DB.
+          await runPrismaCommand({
+            command: ['migrate', 'deploy', '--schema', schemaPath],
+            dbUrl,
+          });
+          log.info('Migration done.');
+
+          // seed
+          // log.info("Seeding...");
+          // await seed(prisma);
+        } catch (e) {
+          log.error(e);
+          process.exit(1);
+        }
+      } else {
+        log.info('Does not need migration');
+      }
+    }
+    // if (!isDev) {
+    //   const exc = exec('npm run migrate', (error, stdout, stderror) => {
+    //     stderror && log.info(stderror);
+    //     stdout && log.info(stdout);
+    //   });
+    //   exc.on('error', (error) => {
+    //     log.info(error);
+    //   });
+    //   exc.on('exit', async (msg) => {
+    //     log.info(msg);
+    //     log.info('Migration completed');
+    //     await bootstrap();
+    //     win.loadFile('./electron/index.html');
+    //     shell.openExternal(`http://localhost:${isDev ? 5173 : 8080}`);
+    //     log.info(app.getAppPath());
+    //   });
+    // } else {
     const win = new BrowserWindow({
       width: 800,
       height: 600,
     });
-    log.info(`Mode:  ${isDev ? 'dev' : 'prod'}`);
-    if (!isDev) {
-      const exc = exec('npm run migrate', (error, stdout, stderror) => {
-        stderror && log.info(stderror);
-        stdout && log.info(stdout);
-      });
-      exc.on('error', (error) => {
-        log.info(error);
-      });
-      exc.on('exit', async (msg) => {
-        log.info(msg);
-        log.info('Migration completed');
-        await bootstrap();
-        win.loadFile('./electron/index.html');
-        shell.openExternal(`http://localhost:${isDev ? 5173 : 8080}`);
-        log.info(app.getAppPath());
-      });
-    } else {
-      await bootstrap();
-      win.loadFile('./electron/index.html');
-      shell.openExternal(`http://localhost:${isDev ? 5173 : 8080}`);
-      log.info(app.getAppPath());
-    }
+    await bootstrap();
+    win.loadFile('./electron/index.html');
+    shell.openExternal(`http://localhost:${isDev ? 5173 : 8080}`);
+    log.info(app.getAppPath());
+    // }
   } catch (error) {
     log.error(error);
   }
